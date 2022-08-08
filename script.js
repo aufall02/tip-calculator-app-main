2c894b4c1263583db33326d130292297cfb688d0
aufall02 <aufaluzumaki@gmail.com>
2022-08-08 10:45:11 +0700

    fisrtCommit

---
+++ script.js
@@ -0,0 +1,26 @@
+const input = document.querySelectorAll('input');
+const span = document.querySelectorAll('span');
+const amount = document.getElementById('amount');
+const btn = document.querySelectorAll('.option');
+
+
+//melakukan prosen calculasi untuk tip
+btn.forEach(button => {
+    button.addEventListener('click', () => {
+        isNaN(Number(input[0].value))?span[0].innerText =0.00:span[0].innerText = (Number((button.value)*input[0].value)).toFixed(2);
+    })
+  });
+input[1].onchange = () => span[0].innerText = ((input[1].value/100)*input[0].value).toFixed(2);
+input[2].onchange = () => span[1].innerText = (Number(span[0].innerText)*input[2].value).toFixed(2);
+  //untuk test apakah inputan angka/bukan
+input.forEach(input => {
+      input.oninput = () => isNaN(Number(input.value))?clear():console.log('ini angka');
+    })
+
+function clear(){
+    span[0].innerText = '0.00';
+    span[1].innerText = '0.00';
+    input[0].value = 0
+    input[1].value = null
+    input[2].value = 0
+}
